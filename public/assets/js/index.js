db62ee16841997027814652a271f708f92327000
michael Means <colemeans51@gmail.com>
2022-04-04 19:55:37 -0500

    save

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -191,4 +191,6 @@ if (window.location.pathname === '/notes') {
   noteText.addEventListener('keyup', handleRenderSaveBtn);
 }
 
-getAndRenderNotes()
+getAndRenderNotes();
+
+
--- routes/apiRoutes/index.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const notesRoutes = require('../apiroutes/notesroutes');
-
-router.use(notesRoutes);
-
-module.exports = router
